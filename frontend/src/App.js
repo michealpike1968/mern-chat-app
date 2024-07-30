7fa2b3663096c5f8d26f76395d7c7935fcc15477
michealpike1968 <michealpike1968@gmail.com>
2024-07-30 03:03:15 -0700

    checkpoint 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import { Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import ChatPage from './pages/ChatPage/ChatPage';
 
 function App() {
   return (
     <div className="App">
-     Hello Micheal!!
+      <Route path="/" component={HomePage} exact/>
+      <Route path="/chats" component={ChatPage} exact/>
     </div>
   );
 }
